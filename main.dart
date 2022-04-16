0ed9fd68c5d45d893b1f1b6b57a7667a597c9ebf
Artemov4450 <103350021+Artemov4450@users.noreply.github.com>
2022-04-16 18:53:05 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_todo/pages/home.dart';
+
+void main() => runApp(MyApp());
+  class MyApp extends StatelessWidget{
+
+    @override
+    Widget build(BuildContext context){
+      return(MaterialApp
+
+        home: Scaffold(
+          body: Center(
+            child: Text('ffff'),
+          ),
+        ),
+    );
+    }
+  }
